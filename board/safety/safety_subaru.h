d5bcb060acd005e7d386614a2de538d7f49031f0
Martin Lillepuu <martin@mlp.ee>
2020-07-05 17:20:20 +0300

    Increase brake_pressed threshold to 1.0 to match stock acc

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -80,7 +80,7 @@ static int subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
     // exit controls on rising edge of brake press (Brake_Pedal)
     if (addr == 0x139) {
-      brake_pressed = (GET_BYTES_48(to_push) & 0xFFF0) > 0;
+      brake_pressed = (GET_BYTES_48(to_push) & 0xFFF0) > 1;
     }
   }
   if (valid && (GET_BUS(to_push) == 1)) {
