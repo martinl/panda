931c8ceb340ca526e5344011e276acbbcaa52a2d
Martin Lillepuu <martin@mlp.ee>
2021-08-01 15:43:14 +0300

    update query_fw_nissan.py example

--- examples/query_fw_nissan.py
+++ examples/query_fw_nissan.py
@@ -1,35 +1,106 @@
 #!/usr/bin/env python3
 import time
-import struct
+import argparse
 from panda import Panda
 from hexdump import hexdump
 from panda.python.isotp import isotp_send, isotp_recv
 
-# 0x7e0 = Toyota
-# 0x18DB33F1 for Honda?
+# Nissal Leaf 2018 IC
 
+ecu_type = {
+  0x707: "UNKNOWN",
+  0x70e: "BRAKE",
+  0x73d: "UNKNOWN",
+  0x73f: "VSP",
+  0x740: "UNKNOWN",
+  0x742: "EPS",
+  0x743: "UNKNOWN",
+  0x744: "UNKNOWN",
+  0x745: "BCM",
+  0x746: "TCU",
+  0x747: "MULTI AV",
+  0x75b: "UNKNOWN",
+  0x74d: "IPDM E/R",
+  0x74e: "UNKNOWN",
+  0x752: "UNKNOWN",
+  0x755: "PARKING BRAKE",
+  0x758: "AVM",
+  0x75d: "UNKNOWN",
+  0x766: "UNKNOWN",
+  0x76a: "UNKNOWN",
+  0x784: "MOTOR CONTROL",
+  0x792: "CHARGER",
+  0x796: "UNKNOWN",
+  0x797: "EV/HEV",
+  0x79b: "HV BATTERY",
+  0x79d: "SHIFT",
+  0x7b7: "UNKNOWN",
+  0x7d4: "UNKNOWN",
+  0x18dad0f1: "UNKNOWN",
+}
+
+can_req = {
+  'ecu_fw_version'  : b"\x21\x83",
+  'ecu_serial'      : b"\x21\x84"
+}
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--rxoffset')
+  parser.add_argument('--debug', action='store_true')
+  parser.add_argument('--addr')
+  args = parser.parse_args()
+
+  if args.addr:
+    addrs = [int(args.addr, base=16)]
+  else:
+    addrs = [0x700 + i for i in range(256)]
+    addrs += [0x18da0000 + (i << 8) + 0xf1 for i in range(256)]
+  results = {}
+
+  if args.rxoffset:
+    rx_offsets = [int(args.rxoffset, base=16)]
+  else:
+    rx_offsets = [0x1, 0x3, 0x8, 0x22, 0x37]
+
+  # for ISO-TP
+  DEBUG=args.debug
+
   panda = Panda()
   panda.set_safety_mode(Panda.SAFETY_ELM327)
   panda.can_clear(0)
 
-  tx_addrs = [0x707, 0x70e, 0x73d, 0x73f, 0x740, 0x742, 0x743, 0x744, 0x745, 0x746, 0x747, 0x74d, 0x752, 0x755, 0x758, 0x75d, 0x784, 0x792, 0x796, 0x797, 0x79b, 0x79d, 0x7b7, 0x7d4]
-  rx_addrs = [0x727, 0x70f, 0x73e, 0x761, 0x760, 0x762, 0x763, 0x764, 0x765, 0x783, 0x767, 0x76d, 0x772, 0x775, 0x778, 0x77d, 0x78c, 0x793, 0x7b6, 0x79a, 0x7bb, 0x7bd, 0x7ba, 0x7d5]
+  # 21 81 = Get VIN
+  isotp_send(panda, b"\x21\x81", 0x797)
+  resp = isotp_recv(panda, 0x79a)
+  hexdump(resp)
+  print("VIN: %s" % "".join(map(chr, resp[:2])))
 
-  can_msgs = [b"\x21\x01", b"\x21\x02", b"\x21\x04", b"\x21\x06", b"\x21\x09", b"\x21\x10", b"\x21\x1f", b"\x21\x83", b"\x21\x84"]
+  with tqdm(addrs) as t:
+    for addr in t:
+      # skip functional broadcast addrs
+      if addr == 0x7df or addr == 0x18db33f1:
+        continue
+      t.set_description(hex(addr))
 
-  for i in range(len(tx_addrs)):
-    for msg in can_msgs:
-      print(hex(tx_addrs[i]))
+      resp = {}
+      for rx_offset in rx_offsets:
+        for label, req in can_req:
+          isotp_send(panda, req, addr)
+          # request all responses for iso-tp request
+          #panda.can_send(addr, "30000a0000000000", 0)
+          resp[label] = isotp_recv(panda, addr + rx_offset)
+          if args.debug:
+            print(f"{addr} {rx_offset}")
+            hexdump(resp[label])
 
-      panda.send_heartbeat()
-      isotp_send(panda, msg, tx_addrs[i])
-      ret = isotp_recv(panda, rx_addrs[i])
-      hexdump(ret)
+      if resp.keys():
+        results[addr] = resp
 
-      print("%s: %s" % (bytes.hex(msg), "".join(map(chr, ret[:2]))))
-
-      # send nissan custom msg (tester present?)
-      panda.can_send(tx_addr, "3000000000000000", 0)
-      #panda.can_send(tx_addr, "30000a0000000000", 0)
+    if len(results.items()):
+      for addr, resp in results.items():
+        print(f"\n\n*** Results for address 0x{addr:X} ({ecu_type[addr]})***\n\n")
+        for req, dat in resp.items():
+          print(f"{req} {dat}")
+    else:
+      print("no fw versions found!")
